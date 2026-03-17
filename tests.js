a818880fe8d2866fcb931153b6638ddf8fd7f9ca
Felix <188768+fb55@users.noreply.github.com>
2026-03-17 09:09:31 +0000

    refactor!: switch to ESM-only
    - Set type: module, add exports field
    - Convert to ESM export syntax
    - Migrate tests to node:test (test/ directory)
    - Add engines >= 20.19.0
    - Remove Tidelift from FUNDING.yml
    
    BREAKING CHANGE: This package is now ESM-only.

--- tests.js
+++
@@ -1,5 +0,0 @@
-var assert = require("assert"),
-    boolbase = require("./");
-
-assert.strictEqual(boolbase.trueFunc(), true, "should evaluate to true");
-assert.strictEqual(boolbase.falseFunc(), false, "should evaluate to false");
